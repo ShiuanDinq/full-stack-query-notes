5b0f5323c8e61c2406cd2b8ef0c94087fbc250b8
Edmund Hang <edmundhang@ymail.com>
2023-02-24 16:53:32 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { createNote, getNotes, updateNote } from "./request";
+
 const App = () => {
+  const queryClient = useQueryClient();
+  const newNoteMutation = useMutation(createNote, {
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData("notes");
+      queryClient.setQueryData("notes", notes.concat(newNote));
+    },
+  });
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("notes");
+    },
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData("notes");
+      const updatedNotes = notes.map((note) =>
+        note.id === newNote.id ? newNote : note
+      );
+      queryClient.setQueryData("notes", updatedNotes);
+    },
+  });
   const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    newNoteMutation.mutate({ content, important: true });
+  };
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({ ...note, important: !note.important });
+  };
+
+  const result = useQuery("notes", getNotes, {
+    refetchOnWindowFocus: false,
+  });
+
+  if (result.isLoading) {
+    return <div>loading data...</div>;
   }
 
-  const notes = []
+  const notes = result.data;
 
-  return(
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
